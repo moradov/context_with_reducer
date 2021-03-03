4874e1fd2517c01d248de8e0ba4543f1c556b554
moradov <moradazyzy@gmail.com>
2021-03-03 13:07:39 +0100

    add item key

--- src/components/PostItem.js
+++ src/components/PostItem.js
@@ -1,15 +1,16 @@
-import React, { useContext } from 'react';
-
-const PostItem = ({ post: { title, body, id }, deletePost }) => {
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+const PostItem = ({ post: { title, body, id }, deletePost, history }) => {
   return (
     <div>
       <h2> {title} </h2>
       <p> {body} </p>
       <div>
         <button onClick={() => deletePost(id)}> delete </button>
+        <button onClick={() => history.push('edit/' + id)}>edit</button>
       </div>
     </div>
   );
 };
 
-export default PostItem;
+export default withRouter(PostItem);
