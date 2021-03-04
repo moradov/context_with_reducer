ed211a28c3db087a9046d7552f4745424b8bcef0
moradov <moradazyzy@gmail.com>
2021-03-04 12:41:51 +0100

    nav err

--- src/components/UI/Navbar.js
+++ src/components/UI/Navbar.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-const Navbar = () => {
+import { Navbar, Nav } from 'react-bootstrap';
+const Navbar_ = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home </Link>
-        </li>
-        <li>
-          <Link to="/add">add </Link>
-        </li>
-      </ul>
-    </nav>
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="#home">React</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link>
+            <Link to="/">Home </Link>
+          </Nav.Link>
+          <Nav.Link>
+            <Link to="/add">Add </Link>
+          </Nav.Link>
+          {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown> */}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
-export default Navbar;
+export default Navbar_;
